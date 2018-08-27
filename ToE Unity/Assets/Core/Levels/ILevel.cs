4258d47c06d8145fc66ce0517d8cc3f6d24e4380
dv-k <DmitryTkachev@outlook.com>
2018-08-27 19:53:47 +0500

    123

---
+++ ToE Unity/Assets/Core/Levels/ILevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public interface ILevel
+{
+    /// <summary>
+    /// Цена
+    /// </summary>
+    int Price { get; }
+}
