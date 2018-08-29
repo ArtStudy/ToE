e3ca5b399d6eafbb2843baa4b62f21c9aad8b0e4
dv-k <DmitryTkachev@outlook.com>
2018-08-29 21:28:45 +0500

    Только геи не делают комит

---
+++ ToE Unity/GameСreator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GameСreator.ToePac;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameСreator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var data = new PAC();
+            data.Items.Add(new Item() { Data = "ТЕКСТ ТЕКСТ ТЕКСТ".GenerateStreamFromString(), FileType = FileTypes.Json, Identifier = 123 });
+            data.Items.Add(new Item() { Data = "ТЕКСТ ТЕКСТ".GenerateStreamFromString(), FileType = FileTypes.Json, Identifier = 12345 });
+            data.Items.Add(new Item() { Data = "ТЕКСТ".GenerateStreamFromString(), FileType = FileTypes.Json, Identifier = 12345678 });
+            var r = PAC.GetPAC(PAC.CreatePAC(data));
+            
+
+            foreach(var item in r.Items)
+            {
+                StreamReader reader = new StreamReader( item.Data);
+                string text = reader.ReadToEnd();
+                Debug.WriteLine(item.FileType + " " +item.Identifier  + " " +text);
+            }
+           
+           
+        }
+    }
+}
